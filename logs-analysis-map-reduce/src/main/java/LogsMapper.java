19ebc5f79127cfc6e59577023fd5cf610e5ba562
el-moudni-hicham <hichamelmoudni7@example.com>
2023-10-26 14:59:18 +0100

    c1

---
+++ logs-analysis-map-reduce/src/main/java/LogsMapper.java
@@ -0,0 +1,14 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class LogsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        String[] tokens = value.toString().split(" ");
+        context.write(new Text(tokens[0]), new IntWritable(Integer.valueOf(tokens[8])));
+    }
+}
---
+++ ventes-map-reduce/src/main/java/SalesReducer.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SalesReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+        long sum = 0;
+        for (DoubleWritable value : values) {
+            sum += value.get();
+        }
+        context.write(key, new DoubleWritable(sum));
+    }
+}
